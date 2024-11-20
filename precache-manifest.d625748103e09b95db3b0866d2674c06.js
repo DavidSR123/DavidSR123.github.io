b42a336137d0cedc93110ab7b46351a3c70c675e
DavidSR123 <Willam_david_9@hotmail.com>
2024-11-20 11:40:53 -0500

    Updates

---
+++ precache-manifest.d625748103e09b95db3b0866d2674c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3876760aaa121f38c6e045a9849c4d57",
+    "url": "/DavidSR123.github.io/index.html"
+  },
+  {
+    "revision": "2a12103edf0c88d24f0b",
+    "url": "/DavidSR123.github.io/static/css/main.03122347.chunk.css"
+  },
+  {
+    "revision": "ec748e6cf251c453cc3f",
+    "url": "/DavidSR123.github.io/static/js/2.ebe4c16a.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/DavidSR123.github.io/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "2a12103edf0c88d24f0b",
+    "url": "/DavidSR123.github.io/static/js/main.e0538cb8.chunk.js"
+  },
+  {
+    "revision": "dec9eba3c2a9030933ae",
+    "url": "/DavidSR123.github.io/static/js/runtime~main.f50beab6.js"
+  }
+]);
